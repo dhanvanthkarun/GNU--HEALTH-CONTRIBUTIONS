84a7268bc0b93a0894e9988e471c17bbe5f4f019
Feng Shu <tumashu@163.com>
2023-07-19 10:26:19 +0800

    orthanc/sync_orthanc_server.py: rewrite with argparse.

--- tryton/doc/samples/interfaces/orthanc/sync_orthanc_server.py
+++ tryton/doc/samples/interfaces/orthanc/sync_orthanc_server.py
@@ -19,14 +19,53 @@
 #                    2 : Orthanc Server label not found                 #
 #########################################################################
 
-from proteus import Model, config as pconfig
 import sys
+import argparse
 
-usage = "Usage : sync_orthanc_server <hostname> <port> <user> <password> " \
-        "<dbname> <orthanc_server_label>"
+from proteus import Model
+from proteus import config as pconfig
 
 
-def orthanc_sync():
+def main():
+    options = parse_options()
+    label = options.label
+    connect_service(options)
+    orthanc_sync(label)
+
+def parse_options():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-l', '--label', required=True,
+                        help="Lable of Orthanc server")
+    parser.add_argument('-H', '--hostname', default='localhost',
+                        help="Hostname of GNU Health Service, default=localhost.")
+    parser.add_argument('-p', '--port', default='8000',
+                        help="Port of GNU Health Service, default=8000.")
+    parser.add_argument('-u', '--user', default='admin',
+                        help="User name of GNU Health, default=admin.")
+    parser.add_argument('-P', '--passwd', required=True,
+                        help="Password of GNU Health.")
+    parser.add_argument('-d', '--database', required=True,
+                        help="Database name of GNU Health.")
+
+    return parser.parse_args()
+
+def connect_service(options):
+    hostname = options.hostname
+    port     = options.port
+    user     = options.user
+    passwd   = options.passwd
+    dbname   = options.database
+
+    health_server = 'http://'+user+':'+passwd+'@'+hostname+':'+port+'/'+dbname+'/'
+    
+    print("Connecting to GNU Health Server ...")
+    conf = pconfig.set_xmlrpc(health_server)
+    # Use XML RPC using session
+    #conf = pconfig.set_xmlrpc_session(health_server, username=user, password=passwd)
+    print("Connected!")
+
+def orthanc_sync(orthanc_server_label):
     OrthancConfig = Model.get('gnuhealth.orthanc.config')
 
     # Try to find the orthanc server associated to the provided label
@@ -36,7 +75,7 @@ def orthanc_sync():
     if find_orthanc_server:
         orthanc_server, = find_orthanc_server
     else:
-        print(f"Orthanc Server Label {orthanc_server_label} not found")
+        print(f"Orthanc Server Label: '{orthanc_server_label}' is not found.")
         sys.exit(2)
 
     index = orthanc_server.last
@@ -48,20 +87,6 @@ def orthanc_sync():
 
     orthanc_server.click('do_sync')
 
-if (len(sys.argv) != 7):
-    sys.exit(usage)
 
-hostname = sys.argv[1]
-port = sys.argv[2]
-user = sys.argv[3]
-passwd = sys.argv[4]
-dbname = sys.argv[5]
-orthanc_server_label = sys.argv[6]
-
-health_server = f'http://{user}:{passwd}@{hostname}:{port}/{dbname}/'
-
-print(f"Connecting to GNU Health Server {health_server}")
-conf = pconfig.set_xmlrpc(health_server)
-print("Connected !")
-
-orthanc_sync()
+if __name__ == '__main__':
+    main()
