21f07fbf674e9b44c4ffad245ae125fddec12108
Feng Shu <tumashu@163.com>
2023-08-17 12:01:25 +0800

    health_orthanc: Update template field's help of OrthancWorklistTemplate again

--- tryton/health_orthanc/health_orthanc.py
+++ tryton/health_orthanc/health_orthanc.py
@@ -49,9 +49,9 @@ class OrthancWorklistTemplate(ModelSQL, ModelView):
 
     template = fields.Text(
         "Template", required=True, 
-        help="Genshi syntax template used to create worklist text, \
-        with dump2dcm command of dcmtk help, worklist text file can \
-        be converted to a .wl file.")
+        help="Genshi syntax template used to create worklist text, "
+        "with dump2dcm command of dcmtk help, worklist text file can "
+        "be converted to a .wl file.")
 
     comment = fields.Text('Comment')
 
