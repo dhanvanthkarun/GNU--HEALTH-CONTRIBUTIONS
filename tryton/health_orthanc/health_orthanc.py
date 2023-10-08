4dba049a93d60d54b81dea7f852a68bb9162119b
Feng Shu <tumashu@163.com>
2023-10-08 09:35:37 +0800

    Fix: bug #64761: Locale [zh_cn] does not exist.

--- tryton/health_orthanc/health_orthanc.py
+++ tryton/health_orthanc/health_orthanc.py
@@ -519,8 +519,8 @@ class OrthancServerConfig(ModelSQL, ModelView):
         try:
             d = pendulum.now() - pendulum.instance(self.sync_time)
             return d.in_words(Transaction().language)
-        except TypeError:
-            return f"No correct instance of {self.sync_time}!"
+        except:
+            return d.in_words('en')
 
 
 class OrthancPatient(ModelSQL, ModelView):
