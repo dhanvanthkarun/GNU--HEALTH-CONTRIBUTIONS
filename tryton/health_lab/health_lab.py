2509751733aec4954465f22836d4e4d53ee3f005
Feng Shu <tumashu@163.com>
2024-01-29 13:20:57 +0800

    health_lab: add no_report to report_style.

--- tryton/health_lab/health_lab.py
+++ tryton/health_lab/health_lab.py
@@ -71,6 +71,7 @@ class TestType(ModelSQL, ModelView):
         ('tbl_h_r', 'Table with result column'),
         ('tbl_nh_r', 'Table with result column (no header)'),
         ('no_tbl', 'Do not use table'),
+        ('no_report', 'No report'),
         ], 'Report style', sort=False, select=True)
 
     @staticmethod
