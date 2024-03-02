4387c402a5ded6cccde8c488d5b64b8f9024c8da
Feng Shu <tumashu@163.com>
2024-03-02 08:16:02 +0800

    gnuhealth-data-import.py: fix typo.

--- tryton/scripts/gnuhealth-data-import/gnuhealth-data-import.py
+++ tryton/scripts/gnuhealth-data-import/gnuhealth-data-import.py
@@ -54,7 +54,7 @@ def parse_options():
                         help="Type of imported data, "
                         "its value can be 'patient', 'medicament', "
                         "'produce' or 'labtest' at the moment, "
-                        "override by '__type' field of csv or odt file.")
+                        "override by '_type' field of csv or odt file.")
 
     return parser.parse_args()
 
