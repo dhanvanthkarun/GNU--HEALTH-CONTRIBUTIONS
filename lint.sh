1a250ff1996825a9c67e623d244c70991a8376b9
Gerald Wiese <wiese@gnuhealth.org>
2024-03-05 18:50:46 +0100

    Add comment how to install lint package

--- lint.sh
+++ lint.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+
+# Following uses PyPi package flake8, run "pipx install flake8" to get it
 echo "Running pycodestyle linting"
 pycodestyle .
 printf "\n\n\nRunning pyflakes linting\n"
 pyflakes .
+
+# Following uses PyPI package reuse, run "pipx install reuse" to get it
 printf "\n\n\nRunning reuse linting\n"
 reuse --root tryton/ lint
