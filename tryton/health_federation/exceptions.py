909e28788a62abfc62ed81a4930cfaf9651109c7
Feng Shu <tumashu@163.com>
2024-03-06 14:25:19 +0800

    health_federation: format with autopep8.

--- tryton/health_federation/exceptions.py
+++ tryton/health_federation/exceptions.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from trytond.exceptions import UserError, UserWarning
+from trytond.exceptions import UserError
 
 
 class NeedLoginCredentials(UserError):
