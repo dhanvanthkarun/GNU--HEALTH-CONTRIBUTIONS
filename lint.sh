88c483a2b515015b0765c2bd6463d69fee3017f1
Feng Shu <tumashu@163.com>
2024-03-06 17:03:20 +0800

    lint.sh: Add Usage comment.

--- lint.sh
+++ lint.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Usage:
+# 1. bash lint.sh
+# 2. bash lint.sh |grep -v pyc |grep -v __init__ |grep -v setup
+
 # Following uses PyPi package flake8, run "pipx install flake8" to get it
 echo "Running pycodestyle linting"
 pycodestyle .
