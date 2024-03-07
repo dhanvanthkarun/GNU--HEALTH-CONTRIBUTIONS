c55a6b61b7d33c59cf48878fd7db1c96783322c4
Feng Shu <tumashu@163.com>
2024-03-07 11:11:46 +0800

    __init__.py: add __all__

--- tryton/health_contact_tracing/tests/__init__.py
+++ tryton/health_contact_tracing/tests/__init__.py
@@ -4,3 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .test_health_contact_tracing import suite
+
+
+__all__ = ['suite']
--- tryton/health_federation/tests/__init__.py
+++ tryton/health_federation/tests/__init__.py
@@ -4,3 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from .test_health_federation import suite
+
+
+__all__ = ['suite']
