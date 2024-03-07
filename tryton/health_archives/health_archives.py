9658347efec5475a65e43c0c8264df46d4cd1553
Feng Shu <tumashu@163.com>
2024-03-07 08:32:23 +0800

    health_archives: autopep8.

--- tryton/health_archives/health_archives.py
+++ tryton/health_archives/health_archives.py
@@ -38,7 +38,7 @@ class PaperArchive(ModelSQL, ModelView):
         ('archived', 'Archived'),
         ('borrowed', 'Borrowed'),
         ('lost', 'Lost'),
-        ), 'Status', required=True, sort=False)
+    ), 'Status', required=True, sort=False)
 
     current_location = fields.Many2One(
         'gnuhealth.hospital.unit', 'Current Location',
@@ -70,7 +70,7 @@ class PaperArchive(ModelSQL, ModelView):
             ('patient_unique', Unique(t, t.patient),
                 'The patient history already exists'),
 
-            ]
+        ]
 
     @classmethod
     def search_patient_code(cls, name, clause):
