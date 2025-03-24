1531a65ec581f2b1727fedea6496f7cfca111508
Luis Falcon <falcon@gnuhealth.org>
2025-03-24 01:15:05 +0000

    Use generic procedure model. Add view and menu for patient procedures. Issue #72

---
+++ tryton/health_nursing/health.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from trytond.pool import PoolMeta
+
+__all__ = ['PatientProcedure']
+
+
+class PatientProcedure(metaclass=PoolMeta):
+    __name__ = 'gnuhealth.patient.procedure'
+
+    """
+    Include patient rounding and ambulatory care references to the
+    list of allowed procedures
+    """
+
+    @classmethod
+    def _get_origin(cls):
+        return super(PatientProcedure, cls)._get_origin() + [
+            'gnuhealth.patient.rounding',
+            'gnuhealth.patient.ambulatory_care',
+        ]
