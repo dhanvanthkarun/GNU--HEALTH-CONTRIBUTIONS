f7774fc5542dcf0e5f10fd0dc4a3a96379338b3c
Luis Falcon <falcon@gnuhealth.org>
2025-04-18 23:22:05 +0100

    Task #98 . Initial refactoring for health_history package

--- tryton/health_history/report/patient_evaluation_report.py
+++ tryton/health_history/report/patient_evaluation_report.py
@@ -5,10 +5,10 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import pytz
-from datetime import datetime
+# import pytz
+# from datetime import datetime
 from trytond.pool import Pool
-from trytond.transaction import Transaction
+# from trytond.transaction import Transaction
 from trytond.report import Report
 
 __all__ = ['PatientEvaluationReport']
@@ -18,21 +18,49 @@ class PatientEvaluationReport(Report):
     __name__ = 'patient.evaluation'
 
     @classmethod
-    def parse(cls, report, objects, data, localcontext):
-        Company = Pool().get('company.company')
+    def module_status(cls, module_name):
+        Module = Pool().get('ir.module')
+        module = Module.search([('name', '=', module_name)], limit=1)
+        if (module and module[0].state == 'activated'):
+            print(f"{module_name} module is activated")
+            return True
+        else:
+            print(f"{module_name} module is NOT installed")
+            return False
 
-        timezone = None
-        company_id = Transaction().context.get('company')
-        if company_id:
-            company = Company(company_id)
-            if company.timezone:
-                timezone = pytz.timezone(company.timezone)
+    @classmethod
+    def get_context(cls, records, header, data):
+        context = super(
+            PatientEvaluationReport, cls).get_context(records, header, data)
 
-        dt = datetime.now()
-        localcontext['print_date'] = datetime.astimezone(dt.replace(
-            tzinfo=pytz.utc), timezone)
-        localcontext['print_time'] = localcontext['print_date'].time()
+        # Check if the socioecomics package is installed
+        if (cls.module_status('health_socioeconomics')):
+            context['socioeconomicsmod'] = True
+        else:
+            context['socioeconomicsmod'] = False
 
-        return super(PatientEvaluationReport, cls).parse(report,
-                                                         objects, data,
-                                                         localcontext)
+        # Check if the surgery package is installed
+        if (cls.module_status('health_surgery')):
+            context['surgerymod'] = True
+        else:
+            context['surgerymod'] = False
+
+        # Check if the genetics package is installed
+        if (cls.module_status('health_genetics')):
+            context['geneticsmod'] = True
+        else:
+            context['geneticsmod'] = False
+
+        # Check if the lifestyle package is installed
+        if (cls.module_status('health_lifestyle')):
+            context['lifestylemod'] = True
+        else:
+            context['lifestylemod'] = False
+
+        # Check if the crypto package is installed
+        if (cls.module_status('health_crypto')):
+            context['cryptomod'] = True
+        else:
+            context['cryptomod'] = False
+
+        return context
