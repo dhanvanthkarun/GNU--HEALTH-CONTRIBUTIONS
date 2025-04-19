15d87c23c29500d40c081d0895035fcdedbce576
Feng Shu <tumashu@163.com>
2025-04-19 14:54:39 +0800

    scripts/lint.sh: test direct formatting in fodt files.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -14,5 +14,18 @@ pyflakes .  || exit_status=$?
 printf "\n\n\nRunning reuse linting\n"
 reuse --root tryton/ lint  || exit_status=$?
 
+printf "\n\n\nRunning fodt direct formatting test ...\n\n"
+find "." -type f -name "*.fodt" -print0 | while IFS= read -r -d '' file; do
+    if [[ ! "$file" == *"default_gnuhealth_report_template"* ]] && grep -q "text:span text:style-name=" "$file"; then
+        echo "WARN: Direct formatting found, it MAYBE impact translation ..."
+        echo "1. Open $file"
+        echo "2. Click: Edit -> Select ALL"
+        echo "3. Click: Format -> Clean Direct Formatting"
+        echo "4. Save file."
+        echo ""
+        exist_status=1
+    fi
+done
+
 # Don't exit 0 if we had errors
 exit "${exit_status:-0}"
