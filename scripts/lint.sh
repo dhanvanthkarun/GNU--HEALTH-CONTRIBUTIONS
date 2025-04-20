8b48901b6ee13633a20ed54365c4b358be896b13
Feng Shu <tumashu@163.com>
2025-04-20 08:28:29 +0800

    Improve scripts/lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,9 @@
 # 1. bash scripts/lint.sh
 # 2. bash scripts/lint.sh |grep -v pyc |grep -v __init__ |grep -v setup
 
+SOURCE_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+cd $SOURCE_DIR/..
+
 # Following uses PyPi package flake8, run "pipx install flake8" to get it
 echo "Running pycodestyle linting"
 pycodestyle .  || exit_status=$?
@@ -15,21 +18,21 @@ printf "\n\n\nRunning reuse linting\n"
 reuse --root tryton/ lint  || exit_status=$?
 
 printf "\n\n\nRunning fodt direct formatting test ...\n\n"
-find "." -type f -name "*.fodt" -print0 | while IFS= read -r -d '' file; do
+find $(pwd) -type f -name "*.fodt" -print0 | while IFS= read -r -d '' file; do
     if [[ ! "$file" == *"default_gnuhealth_report_template"* ]]; then
        if grep -q "text:span text:style-name=" "$file" || \
                grep -q "  <text:s/>" "$file" || \
                grep -q "text:span><text:span" "$file" ; then
            echo "* WARN: Direct formatting found, it MAYBE impact translation ..."
            echo ""
-           echo "  1. Open $file"
+           echo "  1. xdg-open $file"
            echo "  2. Click: Edit -> Select ALL"
            echo "  3. Click: Format -> Clean Direct Formatting"
            echo "  4. Save file."
            echo ""
            echo "* More info: https://docs.gnuhealth.org/his/techguide/development/reports.html"
            echo ""
-           exist_status=1
+           exit_status=1
        fi
     fi
 done
