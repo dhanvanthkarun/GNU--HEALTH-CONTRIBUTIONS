9ab28f6fc876d97c460518262fe53ea6429d150f
Luis Falcon <falcon@gnuhealth.org>
2025-04-30 15:14:35 +0100

    Remove health_profile package #102

--- tryton/health_profile/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2008-2024 Luis Falcón <falcon@gnuhealth.org>
-# SPDX-FileCopyrightText: 2011-2024 GNU Solidario <health@gnusolidario.org>
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from .test_health_profile import suite
-
-
-__all__ = ['suite']
